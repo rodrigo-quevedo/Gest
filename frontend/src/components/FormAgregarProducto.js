0af38b26a27cb5133cf375f75b60d7f30c409d68
rodrigo <martinqueve60@gmail.com>
2024-08-25 13:46:35 -0300

    frontend - agregar producto: form onsubmit handler

--- frontend/src/components/FormAgregarProducto.js
+++ frontend/src/components/FormAgregarProducto.js
@@ -2,15 +2,28 @@ import './FormAgregarProducto.css'
 
 const URL_EXPRESS_APP = process.env.REACT_APP_URL_EXPRESS_APP 
 
+const form = document.getElementById("formAgregarProducto")
+
 const handleSubmit = (e) => {
     e.preventDefault();
+
+    fetch(URL_EXPRESS_APP, {
+        method: 'POST',
+        body: JSON.stringify(
+            Object.fromEntries(
+                new FormData(form)
+            )
+        )
+    })
+    .then((res)=> {
+        res.json()
+    })
 }
 
 const AddProductForm = () => {
     return (
         <div className="container">
-            <form action={URL_EXPRESS_APP} method="GET" 
-            onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit} id="formAgregarProducto">
                 <label for="product">Product: </label>
                 <input 
                 type="text" 
