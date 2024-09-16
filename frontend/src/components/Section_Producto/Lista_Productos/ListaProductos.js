09f6cfd9d6ad2e16ff89b59405fab342eadacb47
rodrigo <martinqueve60@gmail.com>
2024-09-16 11:07:15 -0300

    lista productos- Bug RESUELTO: GET infinitos al backend

--- frontend/src/components/Section_Producto/Lista_Productos/ListaProductos.js
+++ frontend/src/components/Section_Producto/Lista_Productos/ListaProductos.js
@@ -6,7 +6,8 @@ const URL_EXPRESS_APP = process.env.REACT_APP_URL_EXPRESS_APP
 let URL_PRODUCTOS = URL_EXPRESS_APP + `/productos`
 
 const ListaProductos = () => {
-    const [productos, setProductos] = useState(null)
+    const [pedirLista, setPedirLista] = useState(true)
+    const [listaProductos, setListaProductos] = useState(<p>No se pudo obtener la lista de productos del servidor</p>)
 
     const getProductos = () => {
         fetch(URL_PRODUCTOS, {
@@ -16,22 +17,27 @@ const ListaProductos = () => {
             Response.json()
             .then((BodyOfResponseJSONString) => {
                 console.log(`Lista JSON sin parsear:`, BodyOfResponseJSONString)
-                setProductos(JSON.parse(BodyOfResponseJSONString))
+                setPedirLista(false)
+                let productos = JSON.parse(BodyOfResponseJSONString)                
+                setListaProductos(
+                    productos.map((producto) => {
+                        return <li className="productoEnLista">{producto.product}</li>
+                    })
+                )
             })
             .catch((err)=> console.log('JS fetch: Error obteniendo el body de la Response', err))
         })
         .catch(err => console.log('JS Fetch: No se pudo hacer la request.', err))
     }
-
-    getProductos()
-
-    const  listaProductos = productos ?
-    productos.map((producto) => {
-        return <li className="productoEnLista">{producto.product}</li>
-    })
-    :
-    <p>No se pudo obtener la lista de productos del servidor</p>
     
+    if (pedirLista) getProductos();
+
+    // if (pedirLista) {
+        // const productos = getProductos()
+        // listaProductos = productos.map((producto) => {
+        //     return <li className="productoEnLista">{producto.product}</li>
+        // })
+    // }
     
 
     return (
