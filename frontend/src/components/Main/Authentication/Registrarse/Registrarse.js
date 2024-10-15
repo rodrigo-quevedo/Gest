8d7b7397121d919d4d25928caadd876eaef27720
rodrigo <martinqueve60@gmail.com>
2024-10-14 21:18:55 -0300

    frontend - styles: pantalla Registrarse, css basico + responsive completo. Falta verificacion de formulario.

--- frontend/src/components/Main/Authentication/Registrarse/Registrarse.js
+++ frontend/src/components/Main/Authentication/Registrarse/Registrarse.js
@@ -1,30 +1,34 @@
-import './Registrarse.module.css'
+import styles from './Registrarse.module.css'
 
 function Registrarse (
 ) {
     document.querySelector('title').innerText = 'Registrarse';
 
     return (
-        <div>
+        <div className={styles.container}>
             <h1>Registrarse</h1>
 
             <form>
-                <div>
+                <div className={styles.inputContainer}>
                     <label for="usuario">Usuario</label>
                     <input type="text" id="usuario"/>
                 </div>
 
-                <div>
+                <div className={styles.inputContainer}>
                     <label for="password">Contraseña</label>
                     <input type="password" id="password" />
                 </div>
 
-                <div>
+                <div className={styles.inputContainer}>
                     <label for="confirmPassword">Repetir contraseña</label>
                     <input type="password" id="confirmPassword" />
                 </div>
 
-                <input type='submit' value='Enviar'/>
+                <input 
+                    type='submit' 
+                    value='Registrarse'
+                    className={styles.submitButton}
+                />
             </form>
         </div>
     )
