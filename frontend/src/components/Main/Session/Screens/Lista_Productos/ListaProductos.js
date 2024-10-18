cb89b80d1c3e311998c46e9b300dc3a51904be4e
rodrigo <martinqueve60@gmail.com>
2024-10-17 23:55:55 -0300

    frontend - styles: searchBox de ListaProductos

--- frontend/src/components/Main/Session/Screens/Lista_Productos/ListaProductos.js
+++ frontend/src/components/Main/Session/Screens/Lista_Productos/ListaProductos.js
@@ -2,6 +2,7 @@ import {useState} from 'react'
 
 import styles from './ListaProductos.module.css'
 
+import { FaSearch } from "react-icons/fa";
 
 function ListaProductos () {
     document.querySelector('title').innerText = 'Lista de productos';
@@ -11,17 +12,40 @@ function ListaProductos () {
     // const URL_EXPRESS_APP = process.env.REACT_APP_URL_EXPRESS_APP 
     // const URL_LISTA_PRODUCTOS = URL_EXPRESS_APP + '/productos'
 
+    const [searchBoxClicked, setSearchBoxClicked] = useState(false)
 
     return (
-        <div>
-            <section>
-                <h1>Lista de productos</h1>
-                <button className={styles.menuButton}
-                >Ingresar nuevos productos</button>
-                <button className={styles.menuButton}
-                >Ingresar venta</button>
-            </section>
-        </div>
+
+        <section className={styles.container}>
+
+            <h1>Lista de productos</h1>
+
+            <form  id="searchBoxForm_ListaProductos">
+                <div 
+                    className={ searchBoxClicked ?
+                        `${styles.searchBox} ${styles.searchBoxSelected}`    
+                    :
+                        styles.searchBox}
+                    onFocus={()=>{setSearchBoxClicked(true)}}
+                    onBlur={()=>setSearchBoxClicked(false)}
+                >
+
+                    <span>
+                        <FaSearch />    
+                    </span>
+
+                    <input 
+                        type='text'
+                        placeholder='Buscar producto'
+                    />
+
+                </div>
+
+            </form>
+
+            <p>Producto - Cantidad - Precio Unitario - Calcular Precio (ingrese cantidad) - Marca - Proveedor</p>
+
+        </section>
         
     )
 }
