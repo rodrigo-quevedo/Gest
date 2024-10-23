77f1a5c5b8e8ff22222598afb39b8fff58bb744e
rodrigo <martinqueve60@gmail.com>
2024-10-23 12:42:02 -0300

    frontend - fix: validación de FormInput (type text) ahora soporta ñ,Ñ y acentos

--- frontend/src/components/componentes_reutilizables/FormInput/FormInput.js
+++ frontend/src/components/componentes_reutilizables/FormInput/FormInput.js
@@ -28,7 +28,7 @@ function FormInput ({
                             name={idInput}
     
                             maxLength='20'
-                            pattern='^[A-Za-z0-9]{0,20}$'
+                            pattern='^[a-zA-ZÀ-ÿñÑ0-9]{0,20}$'
                             title={"Solo son válidos: letras mayúsculas, letras minúsculas y números. NO se aceptan caracteres especiales. Máximo 20 caracteres."}
                         />
                     </div>
@@ -48,7 +48,7 @@ function FormInput ({
                         name={idInput}
 
                         maxLength='50'
-                        pattern='^[A-Za-z0-9 ]{0,50}$'
+                        pattern='^[a-zA-ZÀ-ÿñÑ0-9 ]{0,50}$'
                         title={"Solo son válidos: letras mayúsculas, letras minúsculas, números, y espacios. NO se aceptan caracteres especiales. Máximo 50 caracteres."}
                     />
                 </div>
@@ -70,8 +70,8 @@ function FormInput ({
                         name={idInput}
 
                         maxLength='30'
-                        pattern='^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{10,30}$'
-                        title={"Caracteres válidos: letras mayúsculas, letras minúsculas y números. NO se aceptan caracteres especiales. Debe tener entre 10 y 30 caracteres. Al menos 1 mayúscula, 1 minúscula y 1 número."}
+                        pattern='^(?=.*[a-zñ])(?=.*[A-ZÑ])(?=.*[0-9])[a-zA-Z0-9ñÑ]{10,30}$'
+                        title={"Caracteres válidos: letras mayúsculas, letras minúsculas y números. NO se aceptan caracteres especiales, tampoco acentos o tildes. Debe tener entre 10 y 30 caracteres. Al menos 1 mayúscula, 1 minúscula y 1 número."}
                     />
                 </div>
             )
