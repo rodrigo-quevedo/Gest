5916a0af7e16f5cf1e057b36085c9c1f66bd3996
rodrigo <martinqueve60@gmail.com>
2024-10-23 17:48:03 -0300

    frontend - SearchBox-> fix:autoComplete=off, feature: validacion en el input, refactor: el useEffect se movio del parent component al SearchBox

--- frontend/src/components/estructura/Main/Session/Screens/Lista_Productos/ListaProductos.js
+++ frontend/src/components/estructura/Main/Session/Screens/Lista_Productos/ListaProductos.js
@@ -28,62 +28,6 @@ function ListaProductos () {
     }])
 
 
-    // Esto se activa SOLO cuando el usuario hizo un submit:
-    //funciona así:
-        //1. usuario aprieta ENTER
-        //2. se "cambia" (setea) el state de CLICKED a SUBMIT-> se triggerea solamente, NO SE ACTUALIZA, se actualiza recién cuando hace el re-render
-        //3. fetch() --> esto empieza a trabajar en segundo plano
-        //4. re-render, acá cambian los estilos a la clase .searchBoxSubmitted, la searchbox aparece como loading
-
-        //5. el fetch tira success (.then() ) o error (.catch() ), sea lo que sea, la clase CSS se cambia en ambos casos, pero se cambia al cambiar un STATE (searchBoxState) de SUBMITED a FETCH_SUCCESS
-        //6. se re-renderiza (al cambiar el state)
-        //7. recién ahí llega acá, y como el state ya está en SUBMIT, entonces se activa el blur() y se deja de mostrar la clase de loading
-        //8. blur() activa el evento onBlur()
-    useEffect(()=>{
-        if (searchBoxState === SEARCHBOX_STATE.SUBMIT) {
-            //esto y la condición de 
-            document.getElementById('searchBoxInput').blur()
-
-            // (Cuando tenga el fetch, esto de abajo se puede borrar.)
-            // Esto esta hardcodeado, en realidad debería ser un fetch.
-            // Pero en realidad tampoco hace falta, ya que esto lo puedo poner en el onSubmit del <form> del SearchBox.
-            
-            // setTimeout(()=>{
-            //     //esto también se hace dentro del fetch():
-            //     setSearchBoxState(SEARCHBOX_STATE.FETCH_SUCCESS)
-
-            //     //esto se hace dentro del fetch():
-            //     setListaProductos([
-            //         {
-            //             id: 1,
-            //             producto: 'Arroz Fino 1kg',
-            //             cantidad: 10,
-            //             precio_unitario: 2400.00,
-            //             marca: 'Doscientos hermanos',
-            //             proveedor: 'Almacén Distribuidora'
-            //         },
-            //         {
-            //             id: 2,
-            //             producto: 'Arroz Fino 1kg',
-            //             cantidad: 20,
-            //             precio_unitario: 2000.00,
-            //             marca: 'Sovimandi',
-            //             proveedor: 'Almacén Distribuidora'
-            //         },
-            //         {
-            //             id: 3,
-            //             producto: 'Arroz Integral 1kg',
-            //             cantidad: 8,
-            //             precio_unitario: 3000.00,
-            //             marca: 'Doscientos hermanos',
-            //             proveedor: 'Pritiado Distribuidora'
-            //         },
-            //     ])
-            
-            // }, 3000)
-        }
-    }, [searchBoxState])
-
 
 
     return (
--- frontend/src/components/estructura/Main/Session/Screens/Lista_Productos/SearchBox/SearchBox.js
+++ frontend/src/components/estructura/Main/Session/Screens/Lista_Productos/SearchBox/SearchBox.js
@@ -1,6 +1,9 @@
 // css
 import styles from './SearchBox.module.css'
 
+// react
+import { useEffect } from 'react';
+
 // icons
 import { FaSearch } from "react-icons/fa";
 import { VscLoading } from "react-icons/vsc";
@@ -13,16 +16,37 @@ import elegirSearchboxClass from './elegir_component_class/elegirSearchboxClass'
 import fetchBackend from '../fetch_backend/fetchBackend';
 
 
-
+//Searchbox es un componente que va (preferentemente, pero no es obligatorio) en conjunto con una tabla, debido a que toma una URL, le hace un GET, y el resultado lo guarda en un state (con el parámetro 'setter'), el cual pertenece al parent component. Luego, en ese parent component, se puede pasar el state a un child component, por ejemplo una tabla, para cargar esos datos del GET.
+//En resúmen: SearchBox -> GET -> setter(resultado_del_GET) -> el state del parent component ahora tiene resultado_del_GET -> parent component le pasa resultado_del_GET a otro child component, por ejemplo una tabla.
 function SearchBox({
-    // El state del SearchBox es cross-fetch, osea: no importa la URL, las clases se aplican de igual manera
+    // El state del SearchBox tiene que ir en el parent component para poder cambiar la className de la tabla dependiendo el state del searchbox:
     searchBoxState,
     setSearchBoxState,
-    // La URL si que puede cambiar
-    URL_LISTA_PRODUCTOS,
+    // La URL puede cambiar para mostrar otras cosas en la tabla:
+    URL,
     // Tambien deberia cambiar el data handling, es decir, una vez el fetch tiene éxito, qué se hace con los datos obtenidos con el fetch
-    setListaProductos
+    setter
 }) {
+
+    // Esto se activa SOLO cuando el usuario hizo un submit. Funciona así:
+        //1. usuario aprieta ENTER
+        //2. se "cambia" (setea) el state de CLICKED a SUBMIT-> se triggerea solamente, NO SE ACTUALIZA, se actualiza recién en el paso 4, cuando hace el re-render
+
+        //3. fetch() --> esto empieza a trabajar en segundo plano
+
+        //4. re-render, acá el state está en SUBMIT, y cambian los estilos a la clase .searchBoxSubmitted, la searchbox aparece como loading
+        //5. recordemos que se apretó ENTER en el input, pero ese input sigue estando "focus", es decir, se puede seguir escribiendo y seguir apretando ENTER, habilitando envío masivo de fetch. Otra desventaja es que se aplican los estilos del ONFOCUS. Inmediatamente en el re-render se activa el useEffect, y dentro hacemos el blur.
+        //6. El blur 
+
+        //x. después de un tiempo, el fetch tira success (.then() ) o error (.catch() ), sea lo que sea, la clase CSS se cambia en ambos casos, al cambiar un STATE (searchBoxState) de SUBMIT a FETCH_SUCCESS
+
+    useEffect(()=>{
+        if (searchBoxState === SEARCHBOX_STATE.SUBMIT) {
+            // document.getElementById('searchBoxInput').blur()
+        }
+    }, [searchBoxState])
+
+
     return (
         <form 
         id="searchBoxForm_ListaProductos"
@@ -32,10 +56,10 @@ function SearchBox({
             setSearchBoxState(SEARCHBOX_STATE.SUBMIT)
 
             // Necesito: URL, Lista, StateEstilos
-            // fetchBackend(setSearchBoxState, URL_LISTA_PRODUCTOS, setListaProductos)
+            // fetchBackend(setSearchBoxState, URL, setter)
             
             //fetch de prueba
-            fetchBackend(setSearchBoxState, 'https://jsonplaceholder.typicode.com/users', setListaProductos)
+            fetchBackend(setSearchBoxState, 'https://jsonplaceholder.typicode.com/users', setter)
         }}
     >
             <div 
@@ -51,7 +75,12 @@ function SearchBox({
                     }
                 }}
 
+                //blur solo se activa cuando se pierde focus.
+                //focus solo se logra haciendo TAB o clickeando el searchbox.
                 onBlur={()=>{
+                    //si el usuario clickeó el searchbox (CLICKED) y luego en otro lugar (blur), el searchbox vuelve a DEFAULT.
+                    //pero cuando el searchbox empieza a hacer el fetch, cambia de CLICKED a SUBMIT, y eso activa el useEffect, el cual activa un blur, para que no se pueda seguir escribiendo en el searchbox mientras está en loading el fetch.
+                    //por eso es necesario distinguir el blur cuando el searchbox está en SUBMIT, porque no queremos que el searchbox vuelva a su className default, sino que queremos que permanezca en su className de loading:
                     if (searchBoxState !== SEARCHBOX_STATE.SUBMIT) {
                         setSearchBoxState(SEARCHBOX_STATE.DEFAULT)
                     }
@@ -71,12 +100,22 @@ function SearchBox({
                 }
                 
 
-                <input 
+                <input                 
+                    id="searchBoxInput"
+                    name='searchBoxInput'
+                    
                     type='text'
                     placeholder='Buscar producto'
-                    id="searchBoxInput"
-                    autoComplete='false'
+                    
+                    autoComplete='off'
+                    //Puedo apretar ENTER y que me traiga todo, por lo que required='false', pero no hace falta ponerlo, ya que false es el defaut
+
+                    //esto lo agregué para cuando el usuario hace tab en vez de clickear:
                     onFocus={()=>setSearchBoxState(SEARCHBOX_STATE.CLICKED)}
+
+                    maxLength='50'
+                    pattern='^[a-zA-ZÀ-ÿñÑ0-9 ]{0,50}$'
+                    title={"Solo son válidos: letras mayúsculas, letras minúsculas, números, y espacios. NO se aceptan caracteres especiales. Máximo 50 caracteres."}
                 />
 
             </div>
