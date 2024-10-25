43163f2f3e2f8a502efc2c4119bd4dbef1d36f25
rodrigo <martinqueve60@gmail.com>
2024-10-25 13:16:15 -0300

    frontend - feature: Registrar Ventas

--- frontend/src/components/estructura/Main/Session/Screens/Registrar_Ventas/RegistrarVentas.js
+++ frontend/src/components/estructura/Main/Session/Screens/Registrar_Ventas/RegistrarVentas.js
@@ -1,17 +1,100 @@
-import './RegistrarVentas.module.css'
+//css
+import styles from  './RegistrarVentas.module.css'
+
+//react
+import {useState} from 'react'
+
+//components
+import FormularioReutilizable from '../../../../../componentes_reutilizables/FormularioReutilizable/FormularioReutilizable';
+import FormInput from '../../../../../componentes_reutilizables/FormInput/FormInput';
+import FetchStatusText from '../../../../../componentes_reutilizables/FetchStatusText/FetchStatusText';
+
+//config
+import {FETCH_STATUS} from '../../../../../../config/config'
+import { URL_REGISTRAR_VENTAS } from "../../../../../../config/config"
+const test_URL_REGISTRAR_VENTAS = 'https://httpbin.org/post'
+
 
 
 function RegistrarVentas () {
     document.querySelector('title').innerText = 'Registrar venta';
 
+    // Esto va cambiando según lo que pase en el fetch:
+    const [fetchStatus, setFetchStatus] = useState({
+        status: FETCH_STATUS.SUCCESS,
+        successMessage: 'Producto ingresado con éxito',
+        errorMessage: 'Error: No se pudo ingresar el producto'
+    })
+
     return (
-        <div>
-            <section>
-                <h1>Ingresar una venta</h1>
-                <button>CANCELAR</button>
-            </section>
+
+        <section className={styles.container}>
+
+            <h1>Ingresar una venta</h1>
+      
+            <FormularioReutilizable 
+                    fetchStatus={fetchStatus}
+                    setFetchStatus={setFetchStatus}
+                    fetchURL={test_URL_REGISTRAR_VENTAS}
+                    formInputs={
+                        <>
+                            <FormInput 
+                                idInput='producto'
+                                type='text'
+                                texto='Nombre del producto'
+
+                                required='true'
+                            />
+
+                            <FormInput 
+                                idInput='cantidad'
+                                type='number'
+                                texto='Cantidad'
+
+                                min='0'
+                                max='9999'
+                                esPrecio='false'
+
+                                required='true'
+                            />
+
+                            <FormInput 
+                                idInput='precio_unitario'
+                                type='number'
+                                texto='Precio unitario'
+
+                                min='0'
+                                max='999999999'
+                                esPrecio='true'
+
+                                required='true'
+                            />
+
+                            <FormInput 
+                                idInput='marca'
+                                type='text'
+                                texto='Marca'
+
+                                required='true'
+                            />
+
+                            <FormInput 
+                                idInput='proveedor'
+                                type='text'
+                                texto='Proveedor'
+
+                                required='true'
+                            />
+                        </>
+                    }
+                />
+
+                <FetchStatusText 
+                    fetchStatus={fetchStatus}
+                />
               
-        </div>
+        </section>
+
     )
 
     
