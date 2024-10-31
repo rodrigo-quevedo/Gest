33a5cdffc2a0b8c31245c8e9f35013e8a34beeda
rodrigo <martinqueve60@gmail.com>
2024-10-31 16:17:11 -0300

    backend - refactor: se movio 'reset collections' y 'collections backup' a utils/, se modularizo el model Backups_List

--- backend/src/utils/db_backup/collections_backup_model.js
+++ backend/src/utils/db_backup/collections_backup_model.js
@@ -1,30 +1,4 @@
-//default connection
-const mongoose = require('mongoose')
-
-//backup connection
-const backups = process.env.DB_URL_BACKUPS;
-const Backups_ListConnection = mongoose.createConnection(backups);
-
-const Backups_ListSchema = new mongoose.Schema({
-    id: {
-        type: Number,
-        required: true,
-        min: 0,
-        validate : {
-            validator : Number.isInteger,
-            message: "La ID del backup debe ser un numero entero"
-        }
-    },
-    backupDescription: {
-        type: String,
-        required: true,
-        minLength: 0,
-        maxLength: 80,
-        match: /^[a-zA-ZÀ-ÿñÑ0-9 ]{0,80}$/
-    }
-})
-
-const Backups_ListModel = Backups_ListConnection.model('Backups_List', Backups_ListSchema)
+const Backups_ListModel = require('./Backups_List_Model')
 
 
 
