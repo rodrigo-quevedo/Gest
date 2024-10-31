4c8d41ed6fb8ebd38c5a583a887b882ec2678928
rodrigo <martinqueve60@gmail.com>
2024-10-30 21:23:08 -0300

    backend - db, feature: dropear todas las colecciones (solo para testing)

---
+++ backend/src/tests/models/reset_collections.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const UsuariosModel = require('../../models/Authentication/Usuarios')
+const Historial_ProductosModel = require('../../models/Session/Historial_Productos')
+const Historial_VentasModel = require('../../models/Session/Historial_Ventas')
+const Lista_ProductosModel = require('../../models/Session/Lista_Productos')
+
+const resetAllCollections = async () => {
+    
+    console.log(await UsuariosModel.collection.drop());
+    console.log(await Historial_ProductosModel.collection.drop());
+    console.log(await Historial_VentasModel.collection.drop());
+    console.log(await Lista_ProductosModel.collection.drop());
+
+
+}
+
+
+
+module.exports = resetAllCollections
