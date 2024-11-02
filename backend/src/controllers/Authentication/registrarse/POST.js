0d254733a76a361c28f6c8de74916cc5494c307c
rodrigo <martinqueve60@gmail.com>
2024-11-01 23:51:40 -0300

    backend - feature: inicio POST en route /registrarse

--- backend/src/controllers/Authentication/registrarse/POST.js
+++ backend/src/controllers/Authentication/registrarse/POST.js
@@ -1,6 +1,26 @@
-const POST =  (req, res) => {
-    
+const UsuariosModel = require('../../../models/Authentication/Usuarios')
 
+const POST =  async (req, res) => {
+
+    try {
+        const usuarioCreado = await UsuariosModel.create({
+            usuario: req.body.usuario,
+            password: req.body.password,
+            isDemo: false
+        })
+
+        if (usuarioCreado) {
+            res.send('Server: Usuario creado con exito.')
+        }
+        else {
+            console.log('Server: Error al crear el usuario')
+        }
+
+    }
+    catch (err){
+        console.log(err)
+        res.send(err)
+    }
 }
 
 module.exports = POST
