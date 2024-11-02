71dd00787fdc1e03fc35af542ed70769cede0ed1
rodrigo <martinqueve60@gmail.com>
2024-11-01 23:55:36 -0300

    backend - fix: path de las routes

--- backend/src/config/useRouters.js
+++ backend/src/config/useRouters.js
@@ -1,18 +1,18 @@
 function useRouters (app) {
 
     const registrarseRoute = require('../routes/Authentication/registrarse')
-    app.use('/', registrarseRoute)
+    app.use('/registrarse', registrarseRoute)
     const ingresarRoute = require('../routes/Authentication/ingresar')
-    app.use('/', ingresarRoute)
+    app.use('/ingresar', ingresarRoute)
     const cerrar_sesionRoute = require('../routes/Authentication/cerrar_sesion')
-    app.use('/', cerrar_sesionRoute)
+    app.use('/cerrar_sesion', cerrar_sesionRoute)
 
     const productosRoute = require('../routes/Session/productos')
-    app.use('/', productosRoute)
+    app.use('/productos', productosRoute)
     const historial_productosRoute = require('../routes/Session/historial_productos')
-    app.use('/', historial_productosRoute)
+    app.use('/historial_productos', historial_productosRoute)
     const historial_ventasRoute = require('../routes/Session/historial_ventas')
-    app.use('/', historial_ventasRoute)
+    app.use('/historial_ventas', historial_ventasRoute)
 
 }
 
