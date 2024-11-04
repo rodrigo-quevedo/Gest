cc79c375814d3c2d5a372c1fcdd49c4fbaed6008
rodrigo <martinqueve60@gmail.com>
2024-11-04 14:54:18 -0300

    backend - fix: se agregaron mensajes de error personalizados a la validacion del model Usuarios

--- backend/src/models/Authentication/Usuarios.js
+++ backend/src/models/Authentication/Usuarios.js
@@ -3,25 +3,25 @@ const mongoose = require('mongoose')
 const UsuariosSchema = new mongoose.Schema({
     
     usuario: {
-        type: String,
-        required: true,
-        minLength: 0,
-        maxLength: 20,
-        match: /^[a-zA-ZÀ-ÿñÑ0-9]{0,20}$/,
-        unique: true
+        type: [String, "Este campo debe ser tipo String"],
+        required: [true, "Es obligatorio llenar este campo."],
+        minLength: [0, "La cantidad mínima de caracteres es 0."],
+        maxLength: [20, "La cantidad máxima de caracteres es 20."],
+        match: [/^[a-zA-ZÀ-ÿñÑ0-9]{0,20}$/, "Solo son válidos las letras mayúsculas, las letras minúsculas y los números. NO se aceptan caracteres especiales. Máximo 20 caracteres."],
+        unique: true//este tira un error de MongoDB, no de mongoose, así que lo manejo aparte.
     },
 
     password: {
-        type: String,
-        required: true,
-        minLength: 10,
-        maxLength: 30,
-        match: /^(?=.*[a-zñ])(?=.*[A-ZÑ])(?=.*[0-9])[a-zA-Z0-9ñÑ]{10,30}$/
+        type: [String, "Este campo debe ser tipo String"],
+        required: [true, "Es obligatorio llenar este campo."],
+        minLength: [10, "La cantidad mínima de caracteres es 10."],
+        maxLength: [30, "La cantidad máxima de caracteres es 30."],
+        match: [/^(?=.*[a-zñ])(?=.*[A-ZÑ])(?=.*[0-9])[a-zA-Z0-9ñÑ]{10,30}$/, "Los caracteres válidos son letras mayúsculas, letras minúsculas y números. NO se aceptan caracteres especiales, tampoco acentos o tildes. Debe tener entre 10 y 30 caracteres. Al menos 1 mayúscula, 1 minúscula y 1 número."]
     },
 
     isDemo: {
-        type: Boolean,
-        required: true
+        type: [Boolean, "Este campo debe ser tipo Boolean"],
+        required:  [true, "Es obligatorio llenar este campo."],
     }
     
 })
