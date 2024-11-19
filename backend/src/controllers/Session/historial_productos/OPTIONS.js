f61208626856c7e1c9a56828a756aa853071b88c
rodrigo <martinqueve60@gmail.com>
2024-11-19 13:53:50 -0300

    backend - feature: /historial_productos route y OPTIONS

--- backend/src/controllers/Session/historial_productos/OPTIONS.js
+++ backend/src/controllers/Session/historial_productos/OPTIONS.js
@@ -1,14 +1,15 @@
 const OPTIONS = (req, res) => {
 
     //logs
-    console.log(`${new Date()}: OPTIONS request en '/historial_productos'`)
+    console.log(`OPTIONS request en '/historial_productos': ${new Date()}`)
     console.log(`body de la request:`, req.body)
 
     //CORS
     res.header({
         "Access-Control-Allow-Origin" : process.env.URL_REACT_CLIENT,
         "Access-Control-Allow-Methods" : "GET, POST",
-        "Access-Control-Allow-Headers": "Content-Type"
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Credentials": "true"
     })
     
     //response
--- backend/src/routes/Session/historial_productos.js
+++ backend/src/routes/Session/historial_productos.js
@@ -7,9 +7,15 @@ const GETcontroller = require('../../controllers/Session/historial_productos/GET
 const POSTcontroller = require('../../controllers/Session/historial_productos/POST')
 const OPTIONScontroller = require('../../controllers/Session/historial_productos/OPTIONS')
 
+
+//middleware import
+const isAuthenticated = require('../../middleware/isAuthenticated')
+const cors_basico = require('../../middleware/cors_basico')
+
+
 //routing
 router.get('/', GETcontroller)
-router.post('/', POSTcontroller)
+router.post('/', cors_basico, isAuthenticated, POSTcontroller)
 router.options('/', OPTIONScontroller)
 
 //export
