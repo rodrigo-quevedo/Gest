e4c94c427db706eaac0a163e1ce02933bb4005c0
rodrigo <martinqueve60@gmail.com>
2024-11-20 15:15:56 -0300

    backend - feature, completo: POST de /historial_productos (luego de validar y agregar a Historial_Productos model) actualiza Lista_Productos model

--- backend/src/controllers/Session/historial_productos/POST.js
+++ backend/src/controllers/Session/historial_productos/POST.js
@@ -8,7 +8,7 @@ const POST =  async (req, res) => {
     const userSession = res.locals.sessionInJwtPayload
     console.log(`sesion: `, userSession)
 
-    //validar body de la request:
+//------------------------------------------------validar body de la request:
 
     // PRODUCTO
     if (!req.body.producto) {
@@ -169,14 +169,15 @@ const POST =  async (req, res) => {
         return;
     }
 
-    //insertar doc en Historial_Productos
+//------------------------------------------------insertar doc en Historial_Productos
     try {
         //primero tengo que buscar el usuario
         const UsuariosModel = require('../../../models/Authentication/Usuarios')
         const usuarioEnSession = await UsuariosModel.find({usuario: userSession.usuario})
-        usuarioEncontrado = usuarioEnSession[0]
+        const usuarioEncontrado = usuarioEnSession[0]
         console.log(`usuario encontrado:`, usuarioEncontrado)
-
+        
+        
         if (!usuarioEncontrado?.usuario){
             console.log('No se pudo encontrar el usuario.')
 
@@ -234,17 +235,125 @@ const POST =  async (req, res) => {
                 return;
             }
 
+            
+    
+//------------------------------------------------actualizar model Lista_Productos:
+        
+        const Lista_ProductosModel = require('../../../models/Session/Lista_Productos')
+        const idListaProductos = usuarioEncontrado.idListaProductos
+        const listaProductosUsuario = await Lista_ProductosModel.findById(idListaProductos)//como es byId, me devuelve 1 solo
 
+        let productoPusheadoListaProductos;
+        
+        console.log('listaProductosUsuario', listaProductosUsuario)
+        
+        //valido si se encontró la lista de usuarios en la db:
+        if (!listaProductosUsuario?.listaProductos){
+            console.log('No se pudo cargar la lista de productos para actualizarla')
 
             res.status(400).json({
-                success: true,
-                message: `Producto ingresado: ${pushResult?.historialProductos[0]}`
+                success: false,
+                message: `No se pudo cargar la lista de productos para actualizarla.`
             })
     
             return;
+        }
 
+        let arrayListaProductosUsuario = listaProductosUsuario?.listaProductos
+        console.log('arrayListaProductosUsuario', arrayListaProductosUsuario)
+        
+        //antes de hacer el update, primero tengo que chequear si el array listaProductos tiene ese producto
+        let prodEncontradoEnListaProductos = false;
 
+        arrayListaProductosUsuario.forEach((prodObj)=>{
+            //aca si el array está vacío, el flag queda en false
+            if (prodObj.producto === pushObj.producto) {
+                prodEncontradoEnListaProductos = prodObj//acá si es true le puedo mandar el objeto del producto dentro de la lista producto
+                
+                //aca no puedo actualizar el array directamente porque es un forEach()
+            }
+        })
+        
+        // si no lo tiene, agregarlo
+        if (prodEncontradoEnListaProductos === false) {
 
+            console.log('no se encontro el producto en la lista de productos')
+            //aca si hago el push nomas
+            arrayListaProductosUsuario.push({
+                producto: pushObj.producto,
+                cantidad: pushObj.cantidad,
+                precio_unitario: pushObj.precio_unitario,
+                marca: pushObj.marca,
+                proveedor: pushObj.proveedor
+            })
+            //save
+            productoPusheadoListaProductos = await listaProductosUsuario.save()
+
+            console.log('productoPusheadoListaProductos', productoPusheadoListaProductos)
+
+            if (!productoPusheadoListaProductos?.listaProductos){
+                console.log('No se pudo cargar la lista de productos para actualizarla')
+
+                res.status(400).json({
+                    success: false,
+                    message: `No se pudo cargar la lista de productos para actualizarla.`
+                })
+        
+                return;
+            }
+            
+        }
+        else {
+            //en cambio, si el producto ya existe en array listaProductos, entonces tengo que hacer un update
+                console.log('si se encontró el producto en la lista de productos')
+
+                //update
+                // actualizar el array
+                arrayListaProductosUsuario = arrayListaProductosUsuario.map((productoObj)=>{
+                    if(productoObj.producto === pushObj.producto) {
+                        console.log('producto encontrado,','productoObj.cantidad:', productoObj.cantidad, 'pushObj.cantidad:',pushObj.cantidad)
+                        productoObj.cantidad = productoObj.cantidad + pushObj.cantidad
+                        return productoObj
+                        
+                        
+                        // return {
+                        //     producto: productoObj.producto,
+                        //     cantidad: productoObj.cantidad + pushObj.cantidad,//simplemente actualizo la cantidad
+                        //     precio_unitario: productoObj.precio_unitario,
+                        //     marca: productoObj.marca,
+                        //     proveedor: productoObj.proveedor
+                        // }
+                    }
+                    return productoObj
+                })
+
+                //save
+                listaProductosUsuario.listaProductos = arrayListaProductosUsuario;
+                productoPusheadoListaProductos = await listaProductosUsuario.save()
+
+                if (!productoPusheadoListaProductos.listaProductos) {
+                    console.log('No se pudo cargar la lista de productos para actualizarla')
+
+                    res.status(400).json({
+                        success: false,
+                        message: `No se pudo cargar la lista de productos para actualizarla.`
+                    })
+            
+                    return;
+                }
+            
+                console.log('productoPusheadoListaProductos', productoPusheadoListaProductos)
+
+        }
+            
+            
+        res.status(400).json({
+            success: true,
+            message: `Producto ingresado: ${pushResult?.historialProductos[0]}`
+        })
+            
+
+        return;
     }
     catch(err){
         console.log(err)
