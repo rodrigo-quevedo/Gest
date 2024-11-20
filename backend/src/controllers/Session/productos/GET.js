bc5b2470784caa2094b3b819b0ee8fe5ac20e1a7
rodrigo <martinqueve60@gmail.com>
2024-11-20 19:27:08 -0300

    backend - feature: GET en /productos, completo. Falta devolver resultados parecidos a lo del searchBoxInput (por ahora solo devuelve exactamente lo que se pone en el SearchBox y además devuelve todo con '')

--- backend/src/controllers/Session/productos/GET.js
+++ backend/src/controllers/Session/productos/GET.js
@@ -1,5 +1,141 @@
-const GET =  (req, res) => {
+//el input del searchbox tiene name "searchBoxInput"
+
+const GET =  async (req, res) => {
     
+    console.log(`GET recibido en /productos: ${new Date()}`)
+    console.log('request body:', req.body)
+
+
+    //validar inputs de la req.body (acá se me complicó porque tengo que dejar pasar el '', ese me lo toma como false en javascript)
+    if (req.body.searchBoxInput === null ||
+        req.body.searchBoxInput === undefined
+    ) {
+        res.status(400).json({
+            success: false,
+            message: 'searchBoxInput: El campo es obligatorio.'
+        })
+
+        return;
+    }
+
+    if (typeof req.body.searchBoxInput !== 'string') {
+        res.status(400).json({
+            success: false,
+            message: `searchBoxInput: '${req.body.searchBoxInput}' es inválido. El campo debe ser tipo String.`
+        })
+
+        return;
+    }
+
+    if (/^[a-zA-ZÀ-ÿñÑ0-9 ]{0,50}$/.test(req.body.searchBoxInput) === false) {
+        res.status(400).json({
+            success: false,
+            message: `searchBoxInput: '${req.body.searchBoxInput}' es inválido. Solo son válidos: letras mayúsculas, letras minúsculas, números, y espacios. NO se aceptan caracteres especiales. Mínimo 0 y máximo 50 caracteres.`
+        })
+
+        return;
+    }
+
+    //servidor responde con la lista:
+    const userSession = res.locals.sessionInJwtPayload
+        //--> tengo que sacar la lista productos para el usuario
+    const UsuariosModel = require('../../../models/Authentication/Usuarios')
+    const usuarioEncontrado = await UsuariosModel.find({usuario: userSession.usuario}).exec()//el findOne() no me estuvo funcionando, por eso uso find() y despues el index [] del array para utilizarlos
+
+    if (!usuarioEncontrado[0]?.idListaProductos) {
+        console.log('No se pudo encontrar al usuario')
+
+        res.status(400).json({
+            success: false,
+            message: `No se pudo encontrar al usuario.`
+        })
+
+        return;
+    }
+
+    const idListaProductos = usuarioEncontrado[0].idListaProductos
+        
+        //--> además, la lista viene limitada por el req.body.searchBoxInput
+    const Lista_ProductosModel = require('../../../models/Session/Lista_Productos')
+    const listaEncontrada = await Lista_ProductosModel.findById(idListaProductos).exec()
+    
+    //puede haber casos en que la listaEncontrada[] tenga length = 0, así que no hay que chequear eso.
+    
+    if (!listaEncontrada?.listaProductos) {
+        console.log('No se pudo encontrar la lista de productos')
+        
+        res.status(400).json({
+            success: false,
+            message: `No se pudo encontrar la lista de productos.`
+        })
+
+        return;
+    }
+    
+        //->aca intento parsear pero no me deja, el return funciona bien, debe ser que es inmutable
+        // let arrayListaProductos = listaEncontrada?.listaProductos;
+
+        //voy a hacer una copia
+        let arrayListaProductos = [];
+        listaEncontrada?.listaProductos.forEach( (el,i) => {
+            arrayListaProductos[i] = el
+        })
+        console.log('primera arrayListaProductos:', arrayListaProductos)
+
+    //tengo que parsear los floats antes de enviarlos.
+    //antes de parsear, me salen en este formato:
+    // "precio_unitario": {
+    //      "$numberDecimal": "2500.00"
+    //}
+    //parse:
+    arrayListaProductos = arrayListaProductos.map((prodObj)=> {
+
+            //no se por qué esto nunca funcionó:
+            // console.log('antes de parsear:', prodObj.precio_unitario, 'despues de parsear:', prodObj.precio_unitario.toString())
+            // // prodObj.precio_unitario = prodObj.precio_unitario.toString()
+            // prodObj.precio_unitario = 'lsdkfjaslkf'
+            // console.log('prodObj.precio_unitario:', prodObj.precio_unitario)
+            
+            // console.log('prodObj despues de parsear:', prodObj)
+            // return prodObj
+
+
+        return {
+            producto: prodObj.producto,
+            cantidad: prodObj.cantidad,
+            precio_unitario: prodObj.precio_unitario.toString(),
+            marca: prodObj.marca,
+            proveedor: prodObj.proveedor
+        }
+    })
+        
+    console.log('Lista de productos parseada:', arrayListaProductos)
+
+    //buscar lo del searchbox dentro del array .listaProductos()
+        //1. armar una REGEXP para devolver resultados similares
+        //2. chequear searchBoxInput = '' para devolver todo
+
+    let arrayResultado = arrayListaProductos.map((prod)=>{
+        if (req.body.searchBoxInput === '') {
+            return prod
+        }
+
+        //aca tengo que armar una REGEXP para devolver resultados similares
+        else if (prod.producto === req.body.searchBoxInput) {
+            return prod
+        }
+
+        return;
+    })
+
+    console.log('arrayResultado:', arrayResultado)
+
+
+    res.status(200).json({
+        success: true,
+        message: arrayResultado
+    })
+    return;
 
 }
 
