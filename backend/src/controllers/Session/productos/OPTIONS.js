68bbe557a19bef6507811ee15711d2696feb9e17
rodrigo <martinqueve60@gmail.com>
2024-11-20 19:22:06 -0300

    backend - feature: route y OPTIONS de /productos

--- backend/src/controllers/Session/productos/OPTIONS.js
+++ backend/src/controllers/Session/productos/OPTIONS.js
@@ -1,14 +1,15 @@
 const OPTIONS = (req, res) => {
 
     //logs
-    console.log(`${new Date()}: OPTIONS request en '/productos'`)
+    console.log(`OPTIONS request en '/productos': ${new Date()}`)
     console.log(`body de la request:`, req.body)
 
     //CORS
     res.header({
         "Access-Control-Allow-Origin" : process.env.URL_REACT_CLIENT,
         "Access-Control-Allow-Methods" : "GET",
-        "Access-Control-Allow-Headers": "Content-Type"
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Credentials": "true"
     })
     
     //response
--- backend/src/routes/Session/productos.js
+++ backend/src/routes/Session/productos.js
@@ -6,8 +6,12 @@ const router = express.Router()
 const GETcontroller = require('../../controllers/Session/productos/GET')
 const OPTIONScontroller = require('../../controllers/Session/productos/OPTIONS')
 
+//middleware import
+const isAuthenticated = require('../../middleware/isAuthenticated')
+const cors_basico = require('../../middleware/cors_basico')
+
 //routing
-router.get('/', GETcontroller)
+router.get('/', cors_basico, isAuthenticated, GETcontroller)
 router.options('/', OPTIONScontroller)
 
 //export
