768f220c9ced73d5133a617966c3f4eacc3d8e16
rodrigo <martinqueve60@gmail.com>
2024-12-01 14:12:32 -0300

    backend - feature: GET controller en /historial_ventas y configuracion de middlewares en route

--- backend/src/controllers/Session/historial_ventas/GET.js
+++ backend/src/controllers/Session/historial_ventas/GET.js
@@ -1,5 +1,87 @@
-const GET =  (req, res) => {
+const GET =  async (req, res) => {
     
+    //logs
+    console.log(`GET en /historial_ventas: ${new Date()}`)
+    console.log('request query:', req.query)
+
+    //req.query validado por middleware
+
+    //usuario
+    const usuarioEncontrado = res.locals.usuarioEncontrado
+
+    //obtener historialVentas[]
+    const Historial_VentasModel = require('../../../models/Session/Historial_Ventas')
+
+    const idHistorialVentas = usuarioEncontrado.idHistorialVentas
+
+    try {
+        const historialVentasEncontrado = await Historial_VentasModel.findById(idHistorialVentas).exec()
+
+        
+        if (! historialVentasEncontrado?.historialVentas) throw new Error("historialVentasEncontrado?.historialVentas is null")
+
+
+        const formatDate = require('../../../utils/format_date/format_date')
+        const historialVentasParseadoYFiltrado = []
+        
+        //parsear decimal128 y date + resultados de busqueda
+        historialVentasEncontrado.historialVentas.forEach(venta =>{
+            //devolver todo
+            if (req.query.searchBoxInput === '') {
+                historialVentasParseadoYFiltrado.push({
+                    producto: venta.producto,
+                    cantidad: venta.cantidad,
+                    precio_unitario: venta.precio_unitario.toString(),
+                    marca: venta.marca,
+                    proveedor: venta.proveedor,
+                    fechaHora: formatDate(venta.fechaHora)
+                })
+            }
+
+            //devolver exact match
+            else if (venta.producto === req.query.searchBoxInput) {
+                historialVentasParseadoYFiltrado.push({
+                    producto: venta.producto,
+                    cantidad: venta.cantidad,
+                    precio_unitario: venta.precio_unitario.toString(),
+                    marca: venta.marca,
+                    proveedor: venta.proveedor,
+                    fechaHora: formatDate(venta.fechaHora)
+                })
+            }
+            
+            //devolver resultados similares
+            else if (venta.producto.includes(req.query.searchBoxInput)){
+                historialVentasParseadoYFiltrado.push({
+                    producto: venta.producto,
+                    cantidad: venta.cantidad,
+                    precio_unitario: venta.precio_unitario.toString(),
+                    marca: venta.marca,
+                    proveedor: venta.proveedor,
+                    fechaHora: formatDate(venta.fechaHora)
+                })
+            }
+
+            //no devolver nada (a continuacion se van a limpiar los null)
+            return;
+        })
+        console.log('historialVentasParseadoYFiltrado: ',historialVentasParseadoYFiltrado)
+
+        res.status(200).json({
+            success: true,
+            message: historialVentasParseadoYFiltrado
+        })
+        return;
+
+    }
+    catch(err){
+        console.log(err)
+
+        res.status(500).json({
+            success: false,
+            message: "DB error. No se pudo encontrar el historial de ventas del usuario en la Base de Datos"
+        })
+    }
 
 }
 
--- backend/src/routes/Session/historial_ventas.js
+++ backend/src/routes/Session/historial_ventas.js
@@ -7,8 +7,17 @@ const GETcontroller = require('../../controllers/Session/historial_ventas/GET')
 const POSTcontroller = require('../../controllers/Session/historial_ventas/POST')
 const OPTIONScontroller = require('../../controllers/Session/historial_ventas/OPTIONS')
 
+
+//middleware import
+const isAuthenticated = require('../../middleware/isAuthenticated')
+const cors_basico = require('../../middleware/cors_basico')
+const validarReqQuerySearchbox = require('../../middleware/validarReqQuerySearchbox')
+const obtenerUsuario = require('../../middleware/obtenerUsuario')
+
+
+
 //routing
-router.get('/', GETcontroller)
+router.get('/', cors_basico, isAuthenticated, validarReqQuerySearchbox, obtenerUsuario,  GETcontroller)
 router.post('/', POSTcontroller)
 router.options('/', OPTIONScontroller)
 
