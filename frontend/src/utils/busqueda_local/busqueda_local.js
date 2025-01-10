6670accab0fcd2f55c34e86f91539a6e02375cf0
rodrigo <martinqueve60@gmail.com>
2025-01-10 16:59:24 -0300

    frontend - refactor: se modularizaron las funciones fetchBackend y busqueda_local para reutilizacion

---
+++ frontend/src/utils/busqueda_local/busqueda_local.js
@@ -0,0 +1,32 @@
+import {SEARCHBOX_STATE} from '../../config/config'
+
+
+function busqueda_local(setSearchBoxState, array, setArrayResultado, productoString) {
+    let arrResultado = array.filter((arrObj)=>{
+
+        // buscar todo
+        if (productoString === ''){
+            return arrObj
+        }
+
+        //busqueda parecida
+        else if (arrObj.producto.toUpperCase().includes(productoString.toUpperCase()) ) {
+            return arrObj
+        }
+
+        // busqueda exacta
+        else if (arrObj.producto.toUpperCase() === productoString.toUpperCase()) {
+            return arrObj
+        }
+
+    })
+
+    // console.log('arrResultado', arrResultado)
+
+    setArrayResultado(arrResultado)
+    setSearchBoxState(SEARCHBOX_STATE.FETCH_SUCCESS)
+
+
+}
+
+export default busqueda_local
---
+++ frontend/src/utils/fetchBackend/fetch_backend/fetchBackend.js
@@ -0,0 +1,68 @@
+import { SEARCHBOX_STATE } from "../../../config/config"
+
+// Nota: Solo hace fetchs GET
+// Nota 2: el metodo GET, segun REST, NO ACEPTA request body, solo url query params (lo que sigue al '?')
+
+function fetchBackend (
+    setSearchBoxState, 
+    URL, 
+    setState, 
+    fetchBody, 
+    setPopupSessionExpired,
+
+    finalFetchStage
+) 
+    {
+    // console.log('Entrando al fetch GET del searchbox');
+    // console.log('fetchBody before parse:', fetchBody)
+    const fetchUrlQuery = new URLSearchParams(fetchBody)
+    // console.log('fetchBody despues del parse:', fetchUrlQuery.toString())
+
+
+    fetch(URL + '?'+ fetchUrlQuery, {
+        method: 'GET',
+        mode: 'cors',
+        credentials: 'include',
+        headers: {     
+            "Accept": "application/json"//accepted format by the browser
+            //Content-Type no me hace falta porque no hay body
+        }
+    })
+    .then(response => {
+        // console.log('respuesta del servidor:', response)
+
+        if (response.status === 401) {
+            setPopupSessionExpired(true)
+        }
+        
+        response.json()
+        .then( json => {
+                // console.log('respuesta del servidor json:', json)
+
+                if (!json.success) {
+                    console.log(`error: ${json.message}`)
+                    return;
+                }
+                
+                // La respuesta de la URL debe venir parseada. El JSON envuelve el body.
+                setState(json.message)
+                if (finalFetchStage){
+                    setSearchBoxState(SEARCHBOX_STATE.FETCH_SUCCESS)
+                }
+            })
+
+        .catch(err=> {
+            console.log('Error al parsear.', err)
+            setSearchBoxState(SEARCHBOX_STATE.FETCH_SUCCESS)
+        });
+
+    })
+
+    .catch(err => {
+        console.log('Error del fetch.', err)
+        setSearchBoxState(SEARCHBOX_STATE.FETCH_SUCCESS)
+    })
+
+}
+
+export default fetchBackend
